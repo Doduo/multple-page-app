6d098a9c0daafd51755cf30273ece53aa669de00
CorderBob <doduox@gmail.com>
2022-12-04 22:45:18 +0800

    添加antd ag-grid Demo

--- src/pages/aaa/aaa.jsx
+++ src/pages/aaa/aaa.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import AAA from "@components/aaa/index";
 import "./index.less";
+import { Button, DatePicker, Space, version } from "antd";
 
 const Aaa = () => {
   // 更新form数据
@@ -11,6 +12,13 @@ const Aaa = () => {
     <div className="a-box">
       <p className="a-p">aaa jsx</p>
       <AAA />
+      <div className="App">
+        <h1>antd version: {version}</h1>
+        <Space>
+          <DatePicker />
+          <Button type="primary">Primary Button</Button>
+        </Space>
+      </div>
     </div>
   );
 };
--- src/pages/bbb/bbb.jsx
+++ src/pages/bbb/bbb.jsx
@@ -1,10 +1,26 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const Bbb = () => {
-  // 更新form数据
-  useEffect(() => {
-    console.log("bbb useEffect 222");
-  }, []);
-  return <div>bbb jsx</div>;
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 720000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+    </div>
+  );
 };
+
 export default Bbb;
